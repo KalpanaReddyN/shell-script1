a116c448a3344a99da1bd9122292ffc0b664b259
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-05 16:03:52 +0100

    current and other scripts

---
+++ 16-currentscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "Before calling the other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+./17-otherscript.sh
---
+++ 17-otherscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="Devops from other script"
+
+echo "Variable value from other script, course: $COURSE"
+echo "Process ID of other script: $$"
