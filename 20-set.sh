608788f2b19886372c29935855e718b5a19d3c5b
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-08 19:14:35 +0100

    set -e command

---
+++ 20-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root access"
+    exit 1
+else
+    echo "You are super user"
+fi
+
+dnf install mysqlll -y  # change this later
+dnf install git -y
+
+echo "is script proceeding"
