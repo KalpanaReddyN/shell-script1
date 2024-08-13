d767def85d7d7a8c08a6c2ff5db7d1687382ac9d
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-13 14:55:17 +0100

    zipping the files

--- 24-zip-example1.sh
+++ 24-zip-example1.sh
@@ -12,7 +12,7 @@ if [ ! -d "$DEST_DIR" ]; then
 fi
 
 # Find old files and zip them
-find "$SOURCE_DIR" -type f -name "$FILE_TYPE" -mtime +14 -print | zip -@ "$DEST_DIR/$IP_FILE_NAME"          # install zip -- sudo yum install zip
+find "$SOURCE_DIR" -type f -name "$FILE_TYPE" -mtime +14 -print | zip -@ "$DEST_DIR/$ZIP_FILE_NAME"          # install zip -- sudo yum install zip
 
 # Optional: Delete the original files after zipping
 # Uncomment the following line if you want to remove the original files
@@ -29,4 +29,15 @@ fi
 
 
 
+#SOURCE_DIR: The directory where the files are located.
+#DEST_DIR: The directory where you want to save the zip file.
+#FILE_TYPE: The type of files you want to zip, such as *.log, *.txt, or any other file extension.
+#ZIP_FILE_NAME: The name of the resulting zip file, which includes the current date.
+#find "$SOURCE_DIR" -type f -name "$FILE_TYPE" -mtime +14:
+
+#-type f: Finds regular files.
+#-name "$FILE_TYPE": Matches files of the specified type (e.g., *.log).
+#-mtime +14: Finds files that were modified more than 14 days ago.
+#-print: Prints the names of the files found, which are then piped to the zip command.
+#zip "$DEST_DIR/$ZIP_FILE_NAME" -@: The -@ option tells zip to read the list of files to be zipped from standard input, which is provided by the find command.
 
