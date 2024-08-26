2680f83d358000ef8da3ef6414508519e7027e43
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-26 16:09:18 +0100

    some-message

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -8,3 +8,19 @@ SUM=$(($NO1+$NO2)) # +,-,*,/,% anything we can use.
 echo "Total of $NO1 and $NO2 is: $SUM"
 
 # sh 07-datatypes.sh 200 400
+
+
+$NO3=$1
+$NO4=$2
+
+MULTIPLY=$(($NO3*$NO4))
+
+echo "Multiply of $NO1 and $NO2 is: $MULTIPLY"
+
+
+$NO1=$1
+$NO2=$2
+
+SUB=$(($NO1-$NO2))
+
+echo "the substraction of $NO1 and $NO2 is: $SUB"
