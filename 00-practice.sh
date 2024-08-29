1a5d1164b1896a02005c15806d61c46f8a4f80e3
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-29 15:37:42 +0100

    some-message

--- 00-practice.sh
+++ 00-practice.sh
@@ -5,16 +5,12 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1) 
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-CHECKROOT(){
-    if [ USERID -ne 0 ]
-    then
-        echo " Please run the script using root access"
-    else
-        echo " You are super user"
-    fi
-}
-
-CHECKROOT
+if [ USERID -ne 0 ]
+then
+    echo " Please run the script using root access"
+else
+    echo " You are super user"
+fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -27,6 +23,7 @@ VALIDATE(){
 
 dnf install mysql -y &>>LOG_FILE
 VALIDATE $? "Installing mysql server"
+
 
 
 
