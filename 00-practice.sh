63d530c0407fb0c9ea8071f2e4f160f8cee7f9f5
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-29 15:44:49 +0100

    some-message

--- 00-practice.sh
+++ 00-practice.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d '.' -f1) 
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+DATE=$(date +%F)
 
-if [ $USERID -ne 0 ]
-then
-    echo " Please run the script using root access"
-else
-    echo " You are super user"
-fi
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo " $2....failure"
-    else
-        echo " $2....Success"
-    fi
-}
-
-dnf install mysql -y &>>LOG_FILE
-VALIDATE $? "Installing mysql server"
+echo "today date is: $DATE" 
 
 
 
