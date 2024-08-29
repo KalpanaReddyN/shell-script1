28f1187fdebc5adac267655c294841b5d1fd8b07
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-29 16:01:35 +0100

    some-message

--- 00-practice.sh
+++ 00-practice.sh
@@ -13,5 +13,20 @@ CHECK_ROOT(){
 
 CHECK_ROOT
 
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "$1....Failure"
+        exit 1
+    else
+        echo "$2....Success"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql-server"
+
+dnf install git -y
+VALIDATE $? "Installing git"
 
 
