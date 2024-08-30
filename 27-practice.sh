aad7c37c67a2cfca6d91a6efd7a054720b0db856
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-30 11:44:32 +0100

    some-message

---
+++ 27-practice.sh
@@ -0,0 +1,20 @@
+set -e
+
+failure(){
+    echo " failed at line $1::$2"
+} 
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run the script with root access"
+else
+    echo "You are super user"
+fi
+
+dnf install mysqll -y
+
+echo "is script proceeding"
