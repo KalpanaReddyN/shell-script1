e725d4a293ba7f4bafbf77723513c8ff9dbb32fa
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-31 13:05:48 +0100

    some-message

--- 27-practice.sh
+++ 27-practice.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=/tmp/app-logs
+LOGS_FOLDER="/var/log/kalpana-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
-if [ -d $SOURCE_DIRECTORY ]
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 26-redirectors.sh package1 package2 ..."
+    exit 1
+}
+
+if [ $# -eq 0 ]
 then
-    echo "Source directory exists"
-else
-    echo "Source directory not exists"
+    USAGE
 fi
 
-FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+echo "Script started executing at: $(date)" &>>$LOG_FILE
 
-echo "deleting files is: $FILES"
+CHECK_ROOT
 
-while IFS= read -r line
+
+for package in $@ # $@ refers to all arguments passed to it
 do
-    echo "deleting file: $line"
-    rm -rf $line
-done <<< $FILES
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.." &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+    fi
+done
+
