89bd839f160f07bf2cab861201a74ff3aee5e46c
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-08-31 12:58:39 +0100

    some-message

--- 27-practice.sh
+++ 27-practice.sh
@@ -2,25 +2,19 @@
 
 SOURCE_DIRECTORY=/tmp/app-logs
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ -d $SOURCE_DIRECTORY ]    #-d: Checks if the specified path is an existing directory.
+if [ -d $SOURCE_DIRECTORY ]
 then
-    echo "source directory exists"
+    echo "Source directory exists"
 else
-    echo "please make sure source directory exists"
+    echo "Source directory not exists"
 fi
 
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
-echo "Files to delete: $FILES"
+echo "deleting files is: $FILES"
 
 while IFS= read -r line
 do
-    echo "Deleting file: $line"
+    echo "deleting file: $line"
     rm -rf $line
-done <<< $FILES
-
+done <<< FILES
