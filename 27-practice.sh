5b2bad935c818b07ad96be57a794d062bd2ca93e
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-01 11:40:49 +0100

    some-message

--- 27-practice.sh
+++ 27-practice.sh
@@ -6,14 +6,13 @@ MESSAGE=""
 
 while IFS= read -r line
 do
-    USAGE=$line | awk -F " " '{print $6F}' | cut -d "%" -f1
-    FOLDER=$line | awk -F " " '{print $NF}'
+    USAGE=$(echo line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo $MESSAGE+="$FOLDER is more than disk $DISK_THRESHOLD, current usage: $USAGE"
+    fi
 done <<< DISK_USAGE
-
-if [ $USAGE -ge $DISK_THRESHOLD ]
-then
-    echo $MESSAGE+="$FOLDER is more than disk $DISK_THRESHOLD, current usage: $USAGE"
-fi
 
 echo "message: $MESSAGE"
 
