3d22bb32ce753c4a161bea51ff277a0b87cb6138
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-01 11:44:12 +0100

    some-message

--- 27-disk-usage-another.sh
+++ 27-disk-usage-another.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=5 #real projects, it is usually 75
+DISK_THRESHOLD=8 #real projects, it is usually 75
 
 
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
