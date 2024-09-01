6e486fd5e21320901db3798483f9bd2a00e54e15
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-01 16:37:30 +0100

    some-message

--- 25-ram-usage.sh
+++ 25-ram-usage.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-RAM_USAGE=$(free -m)
-DISK_THRESHOLD=10
+RAM_USAGE=$(free -m | grep -i "Mem:")
+RAM_THRESHOLD=80
 MESSAGE=""
 
-while IFS= read -r line
-do
-    USAGE=$(echo $line | awk -F " " '{print $3F}')
-    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
-    then
-        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE \n"
-    fi
-done <<< $RAM_USAGE
+TOTAL_RAM=$(echo $RAM_USAGE | awk -F " " '{print $2F}')
+USED_RAM=$(echo $RAM_USAGE | awk -F " " '{print $3F}')
 
+RAM_PERCENTAGE=$(( 100 * USED_RAM / TOTAL_RAM ))
+
+if [ RAM_PERCENTAGE -ge $RAM_THRESHOLD ]
+then
+    $MESSAGE+="RAM usage is above $RAM_THRESHOLD%. current usage: $RAM_PERCENTAGE% ($USED_RAM MB of $TOTAL_RAM MB) "
+    
 echo -e "message: $MESSAGE"
 
-echo "$MESSAGE" | mail -s "Disk Usage Alert" krneelapu@gmail.com  
+#echo "$MESSAGE" | mail -s "RAM Usage Alert" krneelapu@gmail.com  
 # echo "body" | -s "subject" to-mail address.
