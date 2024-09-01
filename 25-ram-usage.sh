7b9f655b83ed317e1ebc066e141c10e63cc26cb0
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-01 16:39:15 +0100

    some-message

--- 25-ram-usage.sh
+++ 25-ram-usage.sh
@@ -9,9 +9,9 @@ USED_RAM=$(echo $RAM_USAGE | awk -F " " '{print $3F}')
 
 RAM_PERCENTAGE=$(( 100 * USED_RAM / TOTAL_RAM ))
 
-if [ RAM_PERCENTAGE -ge $RAM_THRESHOLD ]
+if [ $RAM_PERCENTAGE -ge $RAM_THRESHOLD ]
 then
-    $MESSAGE+="RAM usage is above $RAM_THRESHOLD%. current usage: $RAM_PERCENTAGE% ($USED_RAM MB of $TOTAL_RAM MB) "
+    MESSAGE+="RAM usage is above $RAM_THRESHOLD %. current usage: $RAM_PERCENTAGE % ($USED_RAM MB of $TOTAL_RAM MB) "
     
 echo -e "message: $MESSAGE"
 
