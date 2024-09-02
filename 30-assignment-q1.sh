2e8d68bcced4dcf9b2578129728f26b4d78ab505
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-02 11:35:03 +0100

    some-message

---
+++ 30-assignment-q1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Display top 5 frequntly repeated words
+#Write a shell script that reads a text file and counts the occurrences of each word, display the top 5 most #frequent words along with their counts.
+
